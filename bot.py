c9bda39af60d3d15a23495472cd5cdb45808cc38
Vlad <vladvzozulia@gmail.com>
2026-04-07 19:11:42 +0300

    Delete load_dotenv()

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 import os
-from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
-load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
